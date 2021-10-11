cd950aeaf0e1bfec1c84bf133bc64808dcc86c49
Thanasan Kumdee <6200313@kvis.ac.th>
2021-10-11 21:21:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# จะจีบก็รีบจีบ เดี๋ยวไม่ทัน...
+
+Inspired by : https://twitter.com/SkyandSunn/status/1447135525614219264/photo/1
+
+![จะจีบก็รีบจีบ](https://pbs.twimg.com/media/FBVCFlvVkAQlu_l?format=jpg&name=medium)
+
+# Start
+
+1. Install packages
+
+```
+yarn
+```
+
+2. Start the development server
+
+```
+yarn dev
+```
+
+# Idea for contribution
+
+- Better Design
+- Make the past festivals correspond with current date and further festival: เดี๋ยวไม่ทันฮาโลวีน
+
+Created from https://github.com/sarioglu/svelte-tailwindcss-template template.
---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+  import Tailwindcss from "./Tailwindcss.svelte";
+
+  let festivals = ["มาฆบูชา", "สงกรานต์", "ปีใหม่"];
+</script>
+
+<Tailwindcss />
+<main>
+  <div
+    class="w-screen min-h-screen max-w-full flex flex-col justify-center items-center my-container"
+  >
+    <div class="text-2xl text-center">
+      <h2 class="my-2">จะจีบก็รีบจีบ</h2>
+
+      <div>
+        <h2 class="my-2 inline">เดี๋ยวไม่ทัน</h2>
+        <h2 class="my-2 inline line-through">วาเลนไทน์</h2>
+      </div>
+      {#each festivals as festival, i}
+        {#if i != festivals.length - 1}
+          <h2 class="my-2 ml-20 line-through">{festival}</h2>
+        {:else}
+          <h2 class="my-2 ml-20">{festival}</h2>
+        {/if}
+      {/each}
+    </div>
+    <a
+      class="text-lg mt-36"
+      href="https://twitter.com/SkyandSunn/status/1447135525614219264?s=20"
+      >ที่มา</a
+    >
+  </div>
+</main>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Sarabun&display=swap");
+  * {
+    font-family: "Sarabun", sans-serif;
+  }
+</style>
