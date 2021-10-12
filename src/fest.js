382c4129b2b73431b8f3ce83387e154fcbacefa3
Raghav Maheshwari <75838458+RaghavMahexhwari@users.noreply.github.com>
2021-10-12 12:46:40 +0530

    Update fest.js

--- src/fest.js
+++ src/fest.js
@@ -3,9 +3,21 @@ export const festivals = [{
     date: 14,
     month: 1
 }, {
+    name: "วันตรุษจีน",
+    date: 12,
+    month: 2
+},{
+    name: "มาฆบูชา",
+    date: 26,
+    month: 2
+}, {
     name: "สงกรานต์",
     date: 13,
     month: 3
+},{
+    name: "วันจักรี",
+    date: 6,
+    month: 4
 }, {
     name: "ฮาโลวีน",
     date: 31,
