a14a6f0d582f49b96b61a20ff5d522b41f786a7d
Thanasan Kumdee <6200313@kvis.ac.th>
2021-10-13 11:39:30 +0700

    Add title for twitter card

--- src/App.svelte
+++ src/App.svelte
@@ -3,34 +3,34 @@
   import { sortedfestivals } from "./fest";
 
   var now = new Date();
-  function createDate(date, month){
-    var offset = (date == 1 && month == 1)
-    return new Date(now.getFullYear() + offset, month - 1, date)
+  function createDate(date, month) {
+    var offset = date == 1 && month == 1;
+    return new Date(now.getFullYear() + offset, month - 1, date);
   }
 
   function isBefore(date, month) {
     var day = createDate(date, month);
     return new Date(day.toDateString()) < new Date(new Date().toDateString());
   }
-  
-  function getCurrent(){
+
+  function getCurrent() {
     var len = sortedfestivals.length;
-    for(var i=0; i<len; ++i){
+    for (var i = 0; i < len; ++i) {
       var date = sortedfestivals[i].date;
       var month = sortedfestivals[i].month;
-      if(!isBefore(date, month)){
+      if (!isBefore(date, month)) {
         return sortedfestivals[i].name;
       }
     }
     return sortedfestivals[len - 1].name;
   }
-  
+
   var now_event = getCurrent();
-  
 </script>
 
 <svelte:head>
   <title>จะจีบก็รีบจีบ เดี๋ยวไม่ทัน{now_event}</title>
+  <meta name="twitter:title" content="จะจีบก็รีบจีบ เดี๋ยวไม่ทัน{now_event}" />
 </svelte:head>
 
 <Tailwindcss />
@@ -43,14 +43,14 @@
 
       <div>
         <h2 class="my-2 inline">เดี๋ยวไม่ทัน</h2>
-        
-          {#if isBefore(sortedfestivals[0].date, sortedfestivals[0].month)}
-            <h2 class="my-2 inline line-through"> {sortedfestivals[0].name}.</h2>
-          {:else}
-            <h2 class="my-2 inline">{sortedfestivals[0].name}.</h2>
-          {/if}
+
+        {#if isBefore(sortedfestivals[0].date, sortedfestivals[0].month)}
+          <h2 class="my-2 inline line-through">{sortedfestivals[0].name}.</h2>
+        {:else}
+          <h2 class="my-2 inline">{sortedfestivals[0].name}.</h2>
+        {/if}
       </div>
-      
+
       {#each sortedfestivals as festival, i}
         {#if isBefore(sortedfestivals[i].date, sortedfestivals[i].month) && i != 0}
           <h2 class="my-2 ml-20 line-through non-crossed">{festival.name}</h2>
